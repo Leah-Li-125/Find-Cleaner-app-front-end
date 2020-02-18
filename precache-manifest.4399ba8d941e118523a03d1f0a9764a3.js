5d3d6be2df9da83f9befdbdfe36135ca79a49b93
bannymomo <bannymomo33@gmail.com>
2020-02-18 22:29:08 +1000

    Updates

---
+++ precache-manifest.4399ba8d941e118523a03d1f0a9764a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884669c09d19f49009097a8de8f20749",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fad4bb2e395d7811cf58",
+    "url": "./static/css/main.79ecf822.chunk.css"
+  },
+  {
+    "revision": "b574c3b80fc8b1e209e6",
+    "url": "./static/js/2.8d30b35c.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "./static/js/2.8d30b35c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad4bb2e395d7811cf58",
+    "url": "./static/js/main.98d41400.chunk.js"
+  },
+  {
+    "revision": "e4d6a80ff09c9d0a541e",
+    "url": "./static/js/runtime-main.28807afd.js"
+  },
+  {
+    "revision": "30f6677595f0628da23db7a7156fb895",
+    "url": "./static/media/George.30f66775.JPG"
+  },
+  {
+    "revision": "841392cf149e5b114339cee0d126bf6d",
+    "url": "./static/media/cleaner.841392cf.png"
+  }
+]);
